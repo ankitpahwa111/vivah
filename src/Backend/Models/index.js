0742d7a12e9a367831166f6c5265fce4ec714dae
Ankit Pahwa <ankitpahwa111@gmail.com>
2019-04-10 23:34:03 +0530

    deploy

--- src/Backend/Models/index.js
+++ src/Backend/Models/index.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize({
-    database: process.env.DATABASE_URL,
+const db = new Sequelize(process.env.DATABASE_URL,{
     dialect: 'postgres'
 })
 const couples = db.define('couples', {
